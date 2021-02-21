ab1ba9cdc8f34adadea766267516e438e873f313
Jalim Rabey <123risada@gmail.com>
2021-02-21 15:07:30 -0300

    feat: new colors and custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: colors.yellow,
+      textColor: colors.white,
+      backgroundColor: colors.blueGray,
+      ...colors,
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
